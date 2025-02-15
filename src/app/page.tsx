89985eb703db7d2c045ed22e628a4396bc74b7d1
Adam Neves <adaamangelow@gmail.com>
2025-02-15 16:30:22 -0300

    fix: import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import Slider from "@/components/slider";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
       <Header />
+      <Slider />
     </div>
   );
 }
